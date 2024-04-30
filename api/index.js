3cbbe0cf2ebda85a486a069554303f2c237262f0
Rios marilyne <rios.marilyne@gmail.com>
2024-04-30 19:06:56 +0200

    mongoose et dotenv

--- api/index.js
+++ api/index.js
@@ -1,7 +1,10 @@
 import express from 'express';
-
+import dotenv from 'dotenv';
+import connectDB from './configDb/db.js';
 const app = express();
 
 
+dotenv.config();
+connectDB();
 
 app.listen(3000, () => console.log(`Server listening on port 3000`));
